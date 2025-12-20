2d31b57b233829b8ea6cfa4bb11bed84ae18a53e
CristianVagni <cristianvagni87@gmail.com>
2025-12-20 01:26:18 -0300

    chore: remove build artifacts and generated files from git tracking

--- backend_service/dist/payments/payments.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { UsersService } from '../users/users.service';
-import { PaymentStatus } from '../users/entities/user.entity';
-export declare class PaymentsController {
-    private readonly usersService;
-    constructor(usersService: UsersService);
-    updateStatus(userId: string, status: PaymentStatus): Promise<{
-        message: string;
-    }>;
-}
